f247307477d79fcffbfbc93c059700c040b3c0fa
Sebastian Simon <ssimon@informatik.uni-leipzig.de>
2021-11-15 15:11:31 +0100

    Update sci-kit learn crawler and store results into json files

--- sklearn_crawler.py
+++ sklearn_crawler.py
@@ -2,7 +2,16 @@ import bs4
 import pandas as pd
 import requests
 import json
-    
+from enum import Enum
+
+class ModuleType(Enum):
+    METHOD = 1
+    CLASS = 2
+    DECORATOR =  3
+
+
+print(ModuleType.CLASS)
+
 url = 'https://scikit-learn.org/stable/modules/classes.html'
 module_url = "https://scikit-learn.org/stable/modules/"
 
@@ -11,12 +20,11 @@ def get_page_contents(url):
     return bs4.BeautifulSoup(page.text, "html.parser")
 
 
-def check_if_method(name):
+def check_type(name):
     if name[0].isupper():
-        return False, True
+        return ModuleType.CLASS.name
     else:
-        return True, False
-
+        return ModuleType.METHOD.name
 
 
 soup = get_page_contents(url)
@@ -30,34 +38,34 @@ names = [module["id"] for module in sections if module["id"].startswith(("module
 data = []
 
 for name in names:
-    
     base = soup.find("section", id=name)
     print("==================================================")
     print("name: ", name)
     tables = base.findAll("table", class_="docutils")    
     for table in tables:
-        elements = table.findAll("a", class_="reference internal")
-        for elem in elements:
-                try:
-                    is_method, is_class = check_if_method(elem["title"].split(".")[-1])
-                    module = {"package_name": name,
-                              "full_name": elem["title"], 
-                              "name": elem["title"].split(".")[-1], 
-                              "href": elem["href"],
-                              "is_method": is_method,
-                              "is_class": is_class
-                              }
-                    print(module["name"], module["is_method"])
-                except KeyError:
-                    pass
-                finally:
-                    data.append(module)
+        rows = table.findAll("tr")
+        for tr in rows:
+            entries = tr.findAll("td")
+            entry = entries[0]
+            description = entries[-1].find("p").text
+            
+            element = entry.find("a", class_="reference internal")
+            module_type = check_type(element["title"].split(".")[-1])
+            module = {"package_name": name,
+                      "description": description,
+                      "full_name": element["title"], 
+                      "name": element["title"].split(".")[-1], 
+                      "href": element["href"],
+                      "type": module_type ,
+                      }
+            print(module["name"])
+            data.append(module)
 
 print("number of modules: ", len(names))
 print("number of classes/function", len(data))
 
 
 with open("modules.json", "w") as f:
-    json.dump(data, f)
+     json.dump(data, f, sort_keys=True, indent=4)
 
 
